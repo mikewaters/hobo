21193a817fa507054439e13240e5900c253a7557
mikewaters <robert.waters@gmail.com>
2015-05-05 22:36:07 -0400

    Update setup.py for packaging.

--- setup.py
+++ setup.py
@@ -19,16 +19,32 @@ def get_xdg_hobo_config():
     return xdg_config_home
 
 XDG_HOBO_HOME = get_xdg_hobo_config()
-print XDG_HOBO_HOME
+
 setup(
     name='hobo',
+    version='0.1',
+    description="Wrapper around libguestfs/libvirt for specific workflow.",
+    author="Mike Waters",
+    author_email='robert.waters@gmail.com',
+    url='https://github.com/mikewaters/hobo',
+    packages=[
+        'hobo',
+    ],
+    package_dir={'hobo': 'hobo'},
+    include_package_data=True,
+    license="MIT",
+    zip_safe=False,
+    keywords='hobo',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    ],
     entry_points={
         'console_scripts': ['hobo = hobo.cli:main'],
     },
     install_requires=['pyyaml', 'pyxdg', 'boltons', 'six', 'commandsession'],
-    dependency_links=[
-        "git+ssh://git@github.com/mikewaters/command-session.git#egg=commandsession-0.1",
-    ],
     data_files=[
         (XDG_HOBO_HOME, ['build/hobo.ini'])
     ]
